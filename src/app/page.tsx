bc9219d208ea816d0139e0f923fbaa38856ca887
Tatsuno <ee227059@meiji.ac.jp>
2025-02-22 19:07:10 +0900

    リンタ、フォーマッタ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,24 +9,30 @@ import Heading from "./components/layouts/Heading";
 import { Slider } from "./components/Slider";
 import Footer from "./components/layouts/Footer";
 
-
 export default function Home() {
   return (
     <div>
       <div className="relative w-full max-w-full mx-auto">
-        <Image className="flex items-center justify-center p-6 w-full h-auto max-w-full max-h-full object-contain" width={1500} height={1000} src="/jerryfish.jpg" alt="jerryfish" />
+        <Image
+          className="flex items-center justify-center p-6 w-full h-auto max-w-full max-h-full object-contain"
+          width={1500}
+          height={1000}
+          src="/jerryfish.jpg"
+          alt="jerryfish"
+        />
         <div className="absolute inset-0 flex items-center justify-center ">
           <p className="text-white text-7xl font-bold">Tatsno Shonosuke</p>
         </div>
       </div>
-      
+
       <Heading title="About" />
-      <h1 id="About" className="text-center justify-center font-bold text-3xl p-4 scroll-mt-64">
+      <h1
+        id="About"
+        className="text-center justify-center font-bold text-3xl p-4 scroll-mt-64"
+      >
         Tatsuno Shonosuke / 辰野昇之介
       </h1>
-      <div
-        className="basis-6 max-w-3xl w-full mx-auto  flex items-center justify-center text-left p-6 border-b-4 "
-      >
+      <div className="basis-6 max-w-3xl w-full mx-auto  flex items-center justify-center text-left p-6 border-b-4 ">
         <p>
           明治大学理工学部情報科学科の辰野昇之介です。現在学部3年生でソフトウェア工学研究室に所属しています。
           プログラミング歴は3年、最近は保守性を考慮したコーディングについて関心を持っております。
@@ -34,9 +40,15 @@ export default function Home() {
         </p>
       </div>
       <div className="flex justify-center py-6">
-        <a className="text-blue-500 hover:underline text-lg " target="_blank" rel="noopener noreferrer" href="https://github.com/tSho0418">GitHub</a>
+        <a
+          className="text-blue-500 hover:underline text-lg "
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/tSho0418"
+        >
+          GitHub
+        </a>
       </div>
-      
 
       <Slider />
 
@@ -66,10 +78,7 @@ export default function Home() {
           Backend
         </div>
         <BackendImageList />
-        <div
-          
-          className="basis-6 max-w-3xl w-full mx-auto  flex items-left text-left p-3 border-b-4"
-        >
+        <div className="basis-6 max-w-3xl w-full mx-auto  flex items-left text-left p-3 border-b-4">
           <ul className="list-disc ml-10 space-y-0">
             <li>
               <strong>TypeScript： </strong>
